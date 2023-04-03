cf346ba9272723ee31a313b6ae977824d54305cd
Ddiiro <ddiiro92@gmail.com>
2023-04-03 14:17:07 +0300

    Basic setup for the component

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,7 +4,19 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     
+    <form className="form">
+      <h1>Basic Form Setup</h1>
+      <label for=""> Name :
+        <input type="text"></input>
+      </label>
+      <label for=""> Email :
+        <input type="email"></input>
+      </label>
+      <label for=""> Contact :
+        <input type="contact"></input>
+      </label>
+      <button>Submit</button>
+    </form>
     </div>
   );
 }
