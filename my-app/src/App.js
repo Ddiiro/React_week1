a0dfb991b69dcdc1c7b4f4b4257551e64ffcc2bc
Ddiiro <ddiiro92@gmail.com>
2023-04-03 15:48:59 +0300

    Add styling

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
+  const styles = {
+    main: {
+      display:" flex",
+
+    },
+    form:{
+      width :"50%",
+      margin: "auto",
+      display: "flex",
+      flexDirection: "column",
+      padding: "10px",
+      height: "100%",
+    },
+    input: {
+      width: "90%",
+      padding: "10px",
+      marginTop: "10px",
+      marginBottom: "10px",
+      margin: "auto",
+      color: "red",
+    },
+  };
   return (
     <div className="App">
-    <form className="form">
+    <form className="form" style={styles.form}>
       <h1>Basic Form Setup</h1>
-      <label for=""> Name :
-        <input type="text"></input>
+      <label style={styles.main} for=""> Name :
+        <input type="text" style={styles.input}></input>
       </label>
       <label for=""> Email :
         <input type="email"></input>
